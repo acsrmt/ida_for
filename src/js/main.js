2c0adf180f1e9eb8b00b44db512b20890a58927f
Maxim Zakhvatkin <mv.zakhvatkin@gmail.com>
2017-03-26 00:15:12 +0300

    js validation

--- src/js/main.js
+++ src/js/main.js
@@ -9,9 +9,30 @@ document.addEventListener("DOMContentLoaded", function() {
             toggle = true;
         }
     });
-    window.addEventListener("resize", function() {
-        if (document.documentElement.clientWidth > 640) {
-            document.querySelector(".main-nav__list").classList.remove("toggle");
+});
+
+document.getElementById('paymentForm').addEventListener('submit', function(e) {
+    var regexp_card_num = /^\d{4}$/;
+    var regexp_card_code = /^\d{3}$/;
+    var regexp_card_placeholder = /^[a-zA-Z]{4,}$/;
+    validate(document.paymentForm.CardPlaceholder, regexp_card_placeholder);
+    validate(document.paymentForm.CardCode, regexp_card_code);
+    var cardNum = document.querySelectorAll('.card-num');
+    for (var i = cardNum.length - 1; i >= 0; i--) {
+        console.log(i);
+        validate(cardNum[i], regexp_card_num);
+    }
+
+    function validate(item, regexp) {
+        if (regexp.test(item.value) === false) {
+            item.focus();
+            item.style.outline = '1px solid red';
+            e.preventDefault();
+            return false;
+        } else {
+            item.style.outline = 'none';
         }
-    });
+    }
+    console.log("Всё ок");
+    return true;
 });
